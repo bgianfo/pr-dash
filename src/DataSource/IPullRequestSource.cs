3e1869de9acd55ef8bf97bc9de5c6cfbc039ec33
Brian Gianforcaro <bgianf@microsoft.com>
2019-10-11 16:41:36 -0700

    More removal of usings

--- src/DataSource/IPullRequestSource.cs
+++ src/DataSource/IPullRequestSource.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Microsoft.TeamFoundation.SourceControl.WebApi;
 
 namespace PrDash.DataSource
