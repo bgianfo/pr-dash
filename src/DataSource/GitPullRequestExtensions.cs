6e9d25de76e26eebc0d056024cbe54890c966e04
Brian Gianforcaro <b.gianfo@gmail.com>
2020-04-17 18:02:58 -0700

    DataSource: Add extension method to get latest commit date from PR.

---
+++ src/DataSource/GitPullRequestExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+
+namespace PrDash.DataSource
+{
+    public static class GitPullRequestExtensions
+    {
+        public static DateTime LatestCommitDate(this GitPullRequest pullRequest)
+        {
+            if (pullRequest == null)
+            {
+                throw new ArgumentNullException(nameof(pullRequest));
+            }
+
+            return pullRequest.Commits.OrderBy(c => c.Committer.Date).Last().Committer.Date;
+        }
+    }
+}
